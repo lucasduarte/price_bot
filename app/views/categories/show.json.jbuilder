4126b79e3778794dddade23114ffa37cd3238e5c
Lucas Duarte Anicio <lucasduarte599@gmail.com>
2016-02-20 20:43:48 -0200

    Adds categories resources

---
+++ app/views/categories/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @category, :id, :name, :alert_range, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :products
   resources :websites
   devise_for :users, controllers: { sessions: "users/sessions" }
